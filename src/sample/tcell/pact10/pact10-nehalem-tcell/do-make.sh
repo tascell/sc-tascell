848177027921395727b369a4bb3fd5ae704e2025
hiraisi <hiraisi@kuzuryu>
2010-03-26 21:33:24 +0900

    PACT: finished 1st evaluation

---
+++ src/sample/tcell/pact10/pact10-nehalem-tcell/do-make.sh
@@ -0,0 +1,31 @@
+#!/bin/zsh
+# foeach i in lw clos; do
+#   foreach j in 30 800; do
+#     gmake SC2C_OPT="-D CALL-BOUND=$j -D SEARCH-P-MEM-BARRIER-1=1" -B spanning-$i.c \
+#         && gmake MOPT=-mcpu=niagara2 spanning-$i
+#         && cp spanning-$i spanning-$i-cas-$j
+#     gmake SC2C_OPT="-D CALL-BOUND=$j -D SEARCH-P-MEM-BARRIER-3=1" -B spanning-$i.c \
+#         && gmake MOPT=-mcpu=niagara2 spanning-$i
+#         && cp spanning-$i spanning-$i-membar-$j
+#   done
+# done
+
+foreach i in xcccl; do # xcc xcccl
+  foreach j in 30 800; do
+    gmake SC2C_OPT="-D CALL-BOUND=$j -D SEARCH-P-MEM-BARRIER-1=1" -B spanning-$i.c \
+        && gmake spanning-$i \
+        && cp spanning-$i spanning-$i-cas-$j
+    gmake SC2C_OPT="-D CALL-BOUND=$j -D SEARCH-P-MEM-BARRIER-3=1" -B spanning-$i.c \
+        && gmake spanning-$i \
+        && cp spanning-$i spanning-$i-membar-$j
+  done
+done
+
+## for statistics (i is not important)
+#foreach i in xcccl; do
+#  foreach j in 30; do
+#  gmake SC2C_OPT="-D CALL-BOUND=$j -D SEARCH-P-MEM-BARRIER-3=1 -D STATISTICS=1" -B spanning-$i.c \
+#      && gmake MOPT=-mcpu=ultrasparc spanning-$i \
+#      && cp spanning-$i spanning-$i-membar-$j-statistics
+#  done
+#done
