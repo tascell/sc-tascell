44efcb5bc4d926bd92fd6689ff9d7d690c0331cd
hiraisi <hiraisi@ryujin>
2011-01-22 22:02:36 +0900

    read-body-into-buffer in server.lsp invalidated again (still buggy)

--- src/sample/tcell/mk-hostnames.pl
+++ src/sample/tcell/mk-hostnames.pl
@@ -1,12 +1,14 @@
 #!/usr/bin/perl
 my $STARTHOST=$ARGV[0];
 my $N=$ARGV[1];
+my $LEN;
 
 my $PREFIX;
 my $START;
 if ( $STARTHOST =~ /([^0-9 \t\n\r\f]+)([0-9]+)/ ) {
     $PREFIX = $1;
     $START = $2;
+    $LEN = length $2;
 } else {
     print "Invalid STARTHOST: $STARTHOST\n";
     exit 1;
@@ -15,7 +17,8 @@ if ( $STARTHOST =~ /([^0-9 \t\n\r\f]+)([0-9]+)/ ) {
 my $STR='';
 
 foreach my $i ($START..($START+$N-1)) {
-    $STR .= sprintf ("$PREFIX%03d",$i);
+    $STR .= $PREFIX;
+    $STR .= substr (sprintf ("%0100d",$i), 100-$LEN);
     if ($i<($START+$N-1)) {
         $STR .= '|';
     }
