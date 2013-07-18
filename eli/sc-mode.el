d0815821cc92284c4bbb4728727de8e2d5f2f966
tasuku <tasuku@localhost>
2013-07-18 21:46:49 +0900

    Added Emacs modes for SC/Tascell source files.

---
+++ eli/sc-mode.el
@@ -0,0 +1,166 @@
+(require 'lisp-mode)
+(require 'font-lock)
+
+(defconst sc-font-lock-keywords-1
+  (eval-when-compile
+    `((;;; struct/union/enum specifiers
+       ,(concat
+	 "(\\(struct\\|union\\|enum\\)"	 
+	 "\\([ \t]+"        ; whitespace
+	 "\\(\\sw+\\)\\)?") ; struct/union/enum ID
+       (1 font-lock-keyword-face)
+       (2 font-lock-type-face))
+      (;;; deftype
+       ,(concat
+	 "(\\(deftype\\)" 
+	 "\\([ \t]+"           ; whitespace
+	 "\\(\\sw+\\)\\)?"     ; struct/union/enum ID
+	 "\\([ \t]+"           ; whitespace
+	 "\\(struct\\|union\\|enum\\)\\)?")
+       (1 font-lock-keyword-face)
+       (2 font-lock-type-face)
+       (4 font-lock-keyword-face))
+      (;;; Declarations
+       ,(concat
+	 "(\\(" (regexp-opt
+		 '("def" "decl" "extern" "extern-def"
+		   "extern-decl" "static" "static-def"
+		   "auto" "auto-def" "register" "register-def"))
+	 "\\)\\>"
+	 "[ \t']*"        ; any whitespace
+	 "\\((\\(\\sw+\\)\\|\\(\\sw+\\)\\)\\>?")    ; defined object
+       (1 font-lock-keyword-face)
+       (3 font-lock-function-name-face nil t)
+       (4 font-lock-variable-name-face nil t))
+      (;;; SC-1 declarations
+       ,(concat
+	 "(" (regexp-opt
+	      '("defs" "extern-defs" "static-defs" "auto-defs" "register-defs")
+	      t)
+	 "\\>")
+       (1 font-lock-keyword-face))
+      (;;; Type qualifieers
+       ,(concat
+	 "\\<" (regexp-opt
+		'("void" "char" "signed-char" "unsigned-char"
+		  "short" "signed-short" "unsigned-short"
+		  "int" "signed-int" "unsigned-int"
+		  "long" "signed-long" "unsigned-long"
+		  "long-long" "signed-long-long" "unsigned-long-long"
+		  "float" "double" "long-double")
+		t)
+	 "\\>")
+       (1 font-lock-type-face))
+      ;;; SC preprocessor macro directives
+      (,(concat
+	 "(\\(%defmacro\\)"
+	 "\\([ \t]+"              ; whitespace
+	 "\\(\\sw+\\)\\>\\)?")    ; macro ID
+       (1 font-lock-preprocessor-face)
+       (2 font-lock-function-name-face))
+      (,(concat
+	 "(\\(%defconstant\\|%undef\\|%ifdef\\*?\\|%ifndef\\*?\\)"
+	 "\\([ \t]+"              ; whitespace
+	 "\\(\\sw+\\)\\>\\)?")    ; macro ID
+       (1 font-lock-preprocessor-face)
+       (2 font-lock-variable-name-face))
+      (,(concat
+	 "(" (regexp-opt
+	      '("%include" "%cinclude" "%if" "%if*" "%line" "%error"
+		"%pragma" "%splice" "%rule" "%sc2c" "%output-file"
+		"%string")
+	      t)
+	 "\\>")
+       (1 font-lock-preprocessor-face))
+      ("\\<%else\\>"
+       (1 font-lock-preprocessor-face))
+      ))
+  "Subdued level highlighting for SC modes.")
+
+(defconst sc-font-lock-keywords-2
+  (append sc-font-lock-keywords-1
+   (eval-when-compile
+     `(;; Statement tags
+       (,(concat
+	  "(" (regexp-opt
+	       '("label" "case" "default" "begin" "if" "switch"
+		 "do-while" "goto" "continue" "break" "return"
+		 ;; SC1
+		 "let" "while" "cond" "for" "loop") t)
+	  "\\>")
+	. 1)
+       ;; Type operators, type-qualifiers
+       (,(concat
+	  "(" (regexp-opt
+	       '("array" "struct" "ptr" "fn" "csym::fn") t)
+	  "\\>")
+	. 1)
+       (,(concat
+	  "\\<" (regexp-opt '("va-arg" ":bit"
+			      "const" "restrict" "volatile") t)
+	  "\\>")
+	. 1)
+       ;; Operators, designators
+       (,(concat
+	  "(" (regexp-opt
+	       '("aref-this" "aref" "fref" "fref-this" "->"
+		 "inc" "dec" "init" "sizeof" "++" "--" "ptr"
+		 "mref" "+" "-" "bit-not" "not" "cast"
+		 "*" "/" "%" "<<" ">>" "<" ">" "<=" ">=" "==" "!="
+		 "bit-and" "bit-xor" "bit-or" "and" "or" "if-exp"
+		 "=" "*=" "/=" "%=" "+=" "-=" "<<=" ">>="
+		 "bit-and=" "bit-xor=" "bit-or=" "exps" "c-exp") t)
+	  "\\>")
+	  . 1)
+       ("->" . 0)
+       ;; Character constants, #+/#- macros
+       ("#[\\\\\\+-]\\sw+\\>" 0 font-lock-builtin-face)
+       ;; Constant values.
+       ("\\<:\\sw+\\>" 0 font-lock-builtin-face)
+      )))
+  "Gaudy level highlighting for SC modes.")
+
+(defvar sc-font-lock-keywords sc-font-lock-keywords-1
+  "Default expressions to highlight in SC modes.")
+
+
+(define-derived-mode sc-mode lisp-mode "SC"
+  (setf (car font-lock-defaults)
+	(list 'sc-font-lock-keywords
+	      'sc-font-lock-keywords-1 'sc-font-lock-keywords-2))
+  (let ((tag 'lisp-indent-function))
+    ;; SC constructs
+    (put 'def tag 2)
+    (put 'decl tag 2)
+    (put 'extern tag 2)
+    (put 'extern-def tag 2)
+    (put 'extern-decl tag 2)
+    (put 'static tag 2)
+    (put 'static-def tag 2)
+    (put 'static-decl tag 2)
+    (put 'auto tag 2)
+    (put 'auto-def tag 2)
+    (put 'register tag 2)
+    (put 'register-def tag 2)
+    (put 'begin tag 0)
+    (put 'if tag 2)
+    (put 'switch tag 1)
+    (put 'do-while tag 1)
+    (put 'let tag 1)   ; SC1
+    (put 'for tag 1)   ; SC1
+    (put 'while tag 1) ; SC1
+    (put 'loop tag 0)  ; SC1
+    (put 'cast tag 1)
+    (put 'if-exp tag 2)
+    ;; SC preprocessor directives
+    (put '%cinclude tag 1)
+    (put '%defmacro tag 2)
+    (put '%if tag 2)
+    (put '%ifdef tag 2)
+    (put '%ifndef tag 2)
+    (put '%if* tag 1)
+    (put '%ifdef* tag 1)
+    (put '%ifndef* tag 1)
+    ))
+
+(provide 'sc-mode)
---
+++ eli/tcell-mode.el
@@ -0,0 +1,141 @@
+(load-library "sc-mode")
+
+(defface tcell-keyword-face
+  '((t :inherit font-lock-keyword-face :underline t))
+  "Face for Tascell keywords."
+  :group 'tcell-mode-faces)
+(defvar tcell-keyword-face 'tcell-keyword-face)
+(defface tcell-function-name-face
+  '((t :inherit font-lock-function-name-face :underline t))
+  "Face for Tascell method names."
+  :group 'tcell-mode-faces)
+(defvar tcell-function-name-face 'tcell-function-name-face)
+(defface tcell-variable-name-face
+  '((t :inherit font-lock-variable-name-face :underline t))
+  "Face for Tascell pseudo variable names."
+  :group 'tcell-mode-faces)
+(defvar tcell-variable-name-face 'tcell-variable-name-face)
+(defface tcell-type-face
+  '((t :inherit font-lock-type-face :underline t))
+  "Face for Tascell types."
+  :group 'tcell-mode-faces)
+(defvar tcell-type-face 'tcell-type-face)
+
+
+(defconst *tcell-font-lock-keywords-1
+  (eval-when-compile
+    `((;;; task specifiers
+       ,(concat
+	 "(\\(task\\>\\)"
+	 "\\([ \t]+"        ; whitespace
+	 "\\(\\sw+\\)\\>\\)?") ; task ID
+       (1 tcell-keyword-face t t)
+       (2 font-lock-type-face))
+      (;;; definition of task methods
+       ,(concat
+	 "(\\(def\\)\\>"
+	 "[ \t]*"              ; any whitespace
+	 "(" (regexp-opt
+	      '("task-body" "task-receiver" "task-sender"
+		"rslt-receiver" "rslt-sender") t)
+	 "\\([ \t]+"           ; whitespace
+	 "\\(\\sw+\\)\\)?")    ; task ID
+       (1 font-lock-keyword-face)
+       (2 tcell-function-name-face t t)
+       (6 font-lock-type-face t t))
+      (;;; definition of worker local storage
+       ,(concat
+	 "(\\(def\\)\\>"
+	 "[ \t]*"              ; any whitespace
+	 "\\(worker-data\\)")
+       (1 font-lock-keyword-face)
+       (2 tcell-type-face t t))
+      (;;; definition of an initializer of worker local storage
+       ,(concat
+	 "(\\(def\\)\\>"
+	 "[ \t]*"              ; any whitespace
+	 "\\(worker-init\\)")
+       (1 font-lock-keyword-face)
+       (2 tcell-function-name-face t t))
+      ))
+  )
+
+(defconst tcell-font-lock-keywords-1
+  (append sc-font-lock-keywords-1 *tcell-font-lock-keywords-1)
+  "Subdued level highlighting for Tascell mode.")
+
+(defconst *tcell-font-lock-keywords-2
+  (append *tcell-font-lock-keywords-1
+	  (eval-when-compile
+	    `(;; Worker function tag
+	      (,(concat
+		 "(" (regexp-opt
+		      '("wfn") t)
+		 "\\>")
+	       (1 tcell-keyword-face))
+	      ;; Tascell constructs
+	      (,(concat
+		 "(" (regexp-opt
+		      '("do-two" "do-many*" ":guard" ":put" ":get"
+			"tcell-broadcast" "dynamic-wind"
+			":before" ":body" ":after") t)
+		 "\\>")
+	       (1 tcell-keyword-face t t))
+	      (,(concat
+		 "(\\(" (regexp-opt '("do-many") t)
+		 "\\([ \t]+"           ; whitespace
+		 "\\(for\\)\\>\\)?\\)")
+	       (2 tcell-keyword-face t t)
+	       (4 tcell-keyword-face))
+	      (,(concat
+		 "(\\(handles\\)\\>"
+		 "[ \t]*"              ; any whitespace
+		 "\\(\\<\\sw+\\>\\)")
+	       (1 tcell-keyword-face)
+	       (2 font-lock-type-face))
+	      (,(concat
+		 "\\<" (regexp-opt
+			'(":in" ":out") t)
+		 "\\>")
+	       (1 tcell-keyword-face t t))
+	      (,(concat
+		 "\\<" (regexp-opt
+			'("from" "to") t)
+		 "\\>")
+	       (1 tcell-keyword-face))
+	      ;; pseudo variables
+	      (,(concat
+		 "\\<" (regexp-opt
+			'("PROBABILITY" "WDATA" "WORKER-ID" "this") t)
+		 "\\(\\.\\sw+\\)?\\>")
+	       (1 tcell-variable-name-face))
+	      ))))
+
+(defconst tcell-font-lock-keywords-2
+  (append sc-font-lock-keywords-2 *tcell-font-lock-keywords-2)
+  "Gaudy level highlighting for Tascell mode.")
+
+(defvar tcell-font-lock-keywords tcell-font-lock-keywords-1
+  "Default expressions to highlight in Tascell mode.")
+
+(define-derived-mode tcell-mode sc-mode "Tascell"
+  (setf (car font-lock-defaults)
+	(list 'tcell-font-lock-keywords
+	      'tcell-font-lock-keywords-1 'tcell-font-lock-keywords-2))
+  (let ((tag 'lisp-indent-function))
+    ;; Tascell constructs
+    (put 'do-two tag 2)
+    (put 'do-many tag 6)
+    (put 'do-many* tag 1)
+    (put 'handles tag 1)
+    (put 'dynamic-wind tag 0)
+    (put 'tcell-broadcast tag 1)
+    (put :guard tag 0)
+    (put :get tag 0)
+    (put :put tag 0)
+    (put :before tag 0)
+    (put :body tag 0)
+    (put :after tag 0)
+    ))
+
+(provide 'tcell-mode)
