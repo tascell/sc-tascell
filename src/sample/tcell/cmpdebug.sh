93489758080a6d89fe409ed51fd3b9e9f89c25c1
a0191119 <a0191119@laurel1.kudpc.kyoto-u.ac.jp>
2020-07-11 12:29:05 +0900

    7.11/2

---
+++ src/sample/tcell/cmpdebug.sh
@@ -0,0 +1,15 @@
+
+#============ Options ============
+#QSUB -q gr10034b
+#QSUB -ug gr10034
+#QSUB -W 2:00
+#QSUB -A p=1:t=1:c=1:m=120G
+#QSUB -N cmpdebug
+#============ Shell Script ============
+set -x
+cd $QSUB_WORKDIR
+
+# automatically
+# export OMP_NUM_THREADS=$QSUB_THREADS
+
+mpiexec.hydra ./cmp-gcc -n 1 -v 1 -i "0 10000 -1"
